bd7544eb854d20ed6836628c1dfa2a9f82b897d6
Liad <liad.magen@gmail.com>
2014-04-01 13:57:10 +0200

    Update DL.js

--- js/DL.js
+++ js/DL.js
@@ -1,3 +1,27 @@
+Array.prototype.compare = function (array) {
+    // if the other array is a falsy value, return
+    if (!array)
+        return false;
+
+    // compare lengths - can save a lot of time
+    if (this.length != array.length)
+        return false;
+
+    for (var i = 0, l=this.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!this[i].compare(array[i]))
+                return false;
+        }
+        else if (this[i] != array[i]) {
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;
+        }
+    }
+    return true;
+}
+
 function DeepLearning(gameManager) {
     this.gameManager = gameManager;
     this.lastScore = this.gameManager.score;
@@ -121,7 +145,7 @@ DeepLearning.prototype = {
         this.lastScore = this.gameManager.score;
 
         // no change was done? bad reward
-        if (lastGameGrid.newGrid == this.currentGameGrid.newGrid) {
+        if (lastGameGrid.newGrid.compare(this.currentGameGrid.newGrid)) {
             this.brain.backward(-1);
             return;
         }
